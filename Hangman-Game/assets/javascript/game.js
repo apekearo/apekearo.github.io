94941563d1a626d7fa22e6e60e6a08c6d451eb20
apekearo <apekearo@gmail.com>
2017-04-30 11:34:32 -0500

    making the new Game button work better

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,4 +1,5 @@
-var  words=[
+
+var words=[
 ["R","O","S","E"],
   ["T","U","L","I","P"],
   ["P","E","O","N","Y"],
@@ -6,21 +7,44 @@ var  words=[
   ["Z","I","N","N","I","A"],
   ["D","A","H","L","I","A"]
 ]
-var random = Math.floor((Math.random()*(words.length-1))); 
+var random = Math.floor((Math.random()*(words.length))); 
 
 var answer = words[random]; // the word to guess will be chosen from the array above
 var currentPhrase = new Array(answer.length);
 var error = 0;
+
+var newGameBtn = document.getElementById("newGameBtn");
+
+newGameBtn.addEventListener("touchstart", function() {
+    refreshTheGame();
+})
+
+console.log(answer, currentPhrase);
 
 // every letter in the word is symbolized by an underscore in the guessfield
 for (var i = 0; i < currentPhrase.length; i++){
     currentPhrase[i] = "_ ";
 }
 
+function refreshTheGame() {
+    random = Math.floor((Math.random()*(words.length))); 
+    answer = words[random]; // the word to guess will be chosen from the array above
+    currentPhrase = new Array(answer.length);
+    error = 0;
+    for (var i = 0; i < currentPhrase.length; i++){
+        currentPhrase[i] = "_ ";
+    }
+    printCurrentPhrase();
+    var generateLetter = document.getElementById("generateLetter");
+    generateLetter.innerHTML = "Wrong Letters: "
+    console.log(answer, currentPhrase);
+}
+
 // prints the guessfield
 function printCurrentPhrase(){
-    for (var i = 0; i < currentPhrase.length; i++){
     var gameForm = document.getElementById("gameForm");
+    gameForm.innerHTML = "";
+    for (var i = 0; i < currentPhrase.length; i++){
     var letter = document.createTextNode(currentPhrase[i]);
     gameForm.appendChild(letter);
     }
@@ -41,8 +65,8 @@ var testMark = function(){
     }
     
     //deletes the guessfield and replaces it with the new one
-    var gameForm = document.getElementById("gameForm");
-    gameForm.innerHTML=""; 
+    // var gameForm = document.getElementById("gameForm");
+    // gameForm.innerHTML=""; 
     printCurrentPhrase();
     
     // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
@@ -70,6 +94,8 @@ var testMark = function(){
         window.alert("Oh No! I guess the flower didn't grow! Try Again!");
     }
 }
+function mySubmitFunction(evt) {
+  evt.preventDefault();}
 
 function init(){
     printCurrentPhrase();
